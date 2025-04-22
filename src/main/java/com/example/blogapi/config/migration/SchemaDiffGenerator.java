d6e8b0129f5a73790a6cc7e526ae4af9f8415ca8
Huy Nguyen Nhat <huyntp1103@gmail.com>
2025-04-22 23:05:03 +0700

    feat: update migration generation

--- src/main/java/com/example/blogapi/config/migration/SchemaDiffGenerator.java
+++ src/main/java/com/example/blogapi/config/migration/SchemaDiffGenerator.java
@@ -1,22 +1,22 @@
 package com.example.blogapi.config.migration;
 
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.tool.hbm2ddl.SchemaExport;
-import org.hibernate.tool.schema.TargetType;
-
 import com.example.blogapi.post.model.Post;
 import com.example.blogapi.user.model.User;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.tool.hbm2ddl.SchemaUpdate;
+import org.hibernate.tool.schema.TargetType;
 
 import java.util.EnumSet;
 
-public class MigrationGenerator {
+public class SchemaDiffGenerator {
     public static void main(String[] args) {
         var registry = new StandardServiceRegistryBuilder()
                 .applySetting("hibernate.connection.url", "jdbc:postgresql://localhost:5432/blogdb")
                 .applySetting("hibernate.connection.username", "postgres")
                 .applySetting("hibernate.connection.password", "postgres")
                 .applySetting("hibernate.connection.driver_class", "org.postgresql.Driver")
+                .applySetting("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect")
                 .build();
 
         var metadata = new MetadataSources(registry)
@@ -24,10 +24,9 @@ public class MigrationGenerator {
                 .addAnnotatedClass(Post.class)
                 .buildMetadata();
 
-        new SchemaExport()
+        new SchemaUpdate()
                 .setFormat(true)
-                .setHaltOnError(true)
-                .setOutputFile("src/main/resources/db/migration/V1__initial_schema.sql")
-                .create(EnumSet.of(TargetType.SCRIPT), metadata);
+                .setOutputFile("src/main/resources/db/migration/V2__add_published_column.sql")
+                .execute(EnumSet.of(TargetType.SCRIPT), metadata);
     }
 }
